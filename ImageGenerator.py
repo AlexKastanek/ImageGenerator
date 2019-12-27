480351456506098dd5c51dc5bfe02e3e30fb9f0c
Alex Kastanek <akastanek@nevada.unr.edu>
2019-12-26 22:48:17 -0800

    Initial Commit - Created image generator script

---
+++ ImageGenerator.py
@@ -0,0 +1,173 @@
+# Author: Alex Kastanek
+# Date: November 29, 2019
+# Script: Image Generator
+# Description: Generates an image by constructing a list of
+#   3 functions for the R, G, and B channels of the image.
+#   It does this by randomly generating a set of expressions
+#   separated by a set of operators from a given list of simple
+#   expressions such as cos(PI*x) and sin(PI*x)
+# Reference: This script is highly based on the algorithm seen
+#   in a tutorial, which can be accessed using the link below.
+# https://jeremykun.com/2012/01/01/random-psychedelic-art/
+
+import random, math
+import re
+from PIL import Image
+
+def sinPiX(x):
+    return math.sin(math.pi * x)
+
+def sinPiXY(x, y):
+    return math.sin(math.pi * x * y)
+
+def cosPiX(x):
+    return math.cos(math.pi * x)
+
+def cosPiXY(x, y):
+    return math.cos(math.pi * x * y)
+
+def plotImage(redExpression, greenExpression, blueExpression, pixelsPerUnit = 150):
+    # create a canvas for each color
+    redCanvas = plotColor(redExpression, pixelsPerUnit)
+    greenCanvas = plotColor(greenExpression, pixelsPerUnit)
+    blueCanvas = plotColor(blueExpression, pixelsPerUnit)
+
+    # merge the colors together
+    return Image.merge("RGB", (redCanvas, greenCanvas, blueCanvas))
+
+def plotColor(expression, pixelsPerUnit):
+    # initialize blank canvas
+    canvasWidth = 2 * pixelsPerUnit + 1
+    print("canvasWidth = " + str(canvasWidth))
+    canvas = Image.new("L", (canvasWidth, canvasWidth))
+
+    #DEBUG
+    # x = 1
+    # y = 2
+    # value = evaluateExpression(expression,[x,y])
+    # print("Plot Color - Value: " + str(value))
+    # return
+    
+    # draw the image
+    for it1 in range(canvasWidth):
+        # print("loop lv 0: it1 = " + str(it1))
+        for it2 in range(canvasWidth):
+            # print("loop lv 1: it1 = " + str(it2))
+            # convert pixel location to [-1,1]
+            x = float(it2 - pixelsPerUnit) / pixelsPerUnit
+            y = -float(it1 - pixelsPerUnit) / pixelsPerUnit
+            # print("From plotColor(): ("+str(x)+","+str(y)+")")
+            z = evaluateExpression(expression,[x,y])
+
+            # scale [-1,1] result to [0,255].
+            intensity = int(z * 127.5 + 127.5)
+            canvas.putpixel((it2,it1), intensity)   
+    
+    return canvas
+
+# an expression can be defined as a set of expressions which may or may not be separated by an operator
+def buildExpression(prob = 0.9):
+    #print("From buildExpression(): ("+str(x)+","+str(y)+")")
+    if random.random() < prob:
+        return random.choice([
+            "s(" + buildExpression(prob*prob) + ")",
+            "c(" + buildExpression(prob*prob) + ")",
+            "s(" + buildExpression(prob*prob) + ")*c(" + buildExpression(prob*prob) + ")"])
+    else:
+        return random.choice(["x","y"])
+
+def evaluateExpression(parentExpression, params):
+    # get list of expressions and operators
+    expressionsAndOperators = tokenizeExpression(parentExpression)
+    expressions = expressionsAndOperators[0]
+    operators = expressionsAndOperators[1]
+    # print("Expressions (" + str(len(expressionsAndOperators[0])) + "): " + str(expressionsAndOperators[0]))
+    # print("Operators (" + str(len(expressionsAndOperators[1])) + "): "+ str(expressionsAndOperators[1]))
+
+    # if there are no expressions or operators, then the parent expression contains just a variable, so return the corresponding value
+    if (len(expressions) == 0 and len(operators) == 0):
+        if (parentExpression[0] == 'x'):
+            # print("Returning " + str(params[0]))
+            return params[0]
+        elif (parentExpression[0] == 'y'):
+            # print("Returning " + str(params[1]))
+            return params[1]
+
+    # for each expression, determine the nested expression and evaluate it
+    values = []
+    for expression in expressions:
+        nestedExpression = determineNestedExpression(expression)
+        # print("Nested Expression: " + str(nestedExpression))
+        if (expression[0] == 's'):
+            values.append(sinPiX(evaluateExpression(nestedExpression, params)))
+        elif (expression[0] == 'c'):
+            values.append(cosPiX(evaluateExpression(nestedExpression, params)))
+    
+    # apply the set of operators to the values
+    #TODO: make below functionality actually apply order of operations
+    #NOTE: below functionality currently only multiplies all elements of values together
+    # print("Values: " + str(values))
+    finalValue = values[0]
+    if (len(values) > 1):
+        for value in values[1:]:
+            finalValue *= value
+
+    # print("Final Value: " + str(finalValue))
+    return finalValue
+
+def tokenizeExpression(parentExpression):
+    # will return a wrapper for a list of expressions and a list of operators
+    possibleOperators = ['*']
+    expressions = []
+    operators = []
+    # using a stack to determine what is a full expression
+    finishedExpression = True
+    parenthesisStack = []
+    expression = []
+    for symbol in parentExpression:
+        #print(str(symbol) + " - symbol in possible operators? " + str(symbol in possibleOperators) + ". finished expression ? " + str(finishedExpression) + ".")
+        if ((finishedExpression) and (symbol in possibleOperators) == True):
+            operators.append(symbol)
+        else:
+            expression.append(symbol)
+        if (symbol == '('):
+            parenthesisStack.append(1)
+            finishedExpression = False
+        elif (symbol == ')'):
+            parenthesisStack.pop()
+        #print(str(parenthesisStack))
+        if ((not finishedExpression) and len(parenthesisStack) == 0):
+            finishedExpression = True
+            expressions.append(expression)
+            expression = []
+    expressionsAndOperators = [expressions, operators]
+    return expressionsAndOperators
+
+def determineNestedExpression(expression):
+    nestedExpression = []
+    parenthesisStack = []
+    for symbol in expression:
+        if (symbol == '('):
+            parenthesisStack.append(1)
+            if (len(parenthesisStack) == 1):
+                continue
+        elif (symbol == ')'):
+            parenthesisStack.pop()
+        if (len(parenthesisStack) > 0):
+            nestedExpression.append(symbol)
+    return nestedExpression
+
+def main():
+    random.seed()
+    for it in range (0, 2):
+        redExpression = buildExpression()
+        greenExpression = buildExpression()
+        blueExpression = buildExpression()
+        print("R:"+redExpression)
+        print("G:"+greenExpression)
+        print("B:"+blueExpression)
+        image = plotImage(redExpression, greenExpression, blueExpression, 750)
+        image.save("test" + str(it) + ".png", "PNG")
+
+if __name__ == "__main__":
+    main()
