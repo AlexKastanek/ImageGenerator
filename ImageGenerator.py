c51cbfa19f4c0783d40d4e2f7a694fb4e165be1a
Alex Kastanek <akastanek@nevada.unr.edu>
2019-12-30 22:25:32 -0800

    Fixed a bug in which an incorrect string would be generated

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -87,7 +87,7 @@ def buildExpression(prob = 0.9):
             "s(" + buildExpression(prob*prob) + ")*t(" + buildExpression(prob*prob) + ")"]),
             random.choice(["c(" + buildExpression(prob*prob) + ")*s(" + buildExpression(prob*prob) + ")",
             "c(" + buildExpression(prob*prob) + ")*t(" + buildExpression(prob*prob) + ")"]),
-            random.choice(["t(" + buildExpression(prob*prob) + ")*s(" + buildExpression(prob*prob) + ")"
+            random.choice(["t(" + buildExpression(prob*prob) + ")*s(" + buildExpression(prob*prob) + ")",
             "t(" + buildExpression(prob*prob) + ")*c(" + buildExpression(prob*prob) + ")"])])
     else:
         return random.choice(["x","y"])
